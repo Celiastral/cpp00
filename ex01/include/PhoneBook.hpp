88c7e1fca18f3d7df50fda86564da0e08c162065
Celia <elioandre692@gmail.com>
2024-09-05 22:34:00 +0200

    more updates on phonebook

--- ex01/include/PhoneBook.hpp
+++ ex01/include/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: eandre <eandre@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 14:39:01 by eandre            #+#    #+#             */
-/*   Updated: 2024/09/05 14:43:05 by eandre           ###   ########.fr       */
+/*   Updated: 2024/09/05 22:20:36 by eandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,25 @@
 class Contact
 {
 	public :
-		std::string get_value();
-		void set_value(std::string value);
+		std::string	get_value();
+		void		set_first_name(std::string first_name);
+		void		set_last_name(std::string last_name);
+		void		set_nickname(std::string nickname);
+		void		set_phone_nbr(std::string phone_nbr);
+		void		set_secret(std::string secret);
 	private :
-		std::string value;
+		std::string first_name;
+		std::string last_name;
+		std::string nickname;
+		std::string phone_nbr;
+		std::string secret;
+
+};
+
+class Phonebook
+{
+	public :
+		void	set_contact(Contact contact);
+	private :
+		Contact	contact[8];
 };
--- ex01/srcs/PhoneBook.cpp
+++ ex01/srcs/PhoneBook.cpp
@@ -6,24 +6,57 @@
 /*   By: eandre <eandre@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 14:24:10 by eandre            #+#    #+#             */
-/*   Updated: 2024/09/05 14:54:55 by eandre           ###   ########.fr       */
+/*   Updated: 2024/09/05 22:25:21 by eandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/PhoneBook.hpp"
 
-std::string	Contact::get_value()
+void	Phonebook::set_contact(Contact contact)
 {
-	return (Contact::value);
+	
 }
 
-void	Contact::set_value(std::string value)
+std::string	Contact::get_value()
 {
-	Contact::value = value;
+	std::cout << Contact::first_name << std::endl;
+	std::cout << Contact::last_name << std::endl;
+	std::cout << Contact::nickname << std::endl;
+	std::cout << Contact::phone_nbr << std::endl;
+	std::cout << Contact::secret << std::endl;
+	return (Contact::secret);
+}
+
+void	Contact::set_first_name(std::string first_name)
+{
+	Contact::first_name = first_name;
+}
+
+void	Contact::set_last_name(std::string last_name)
+{
+	Contact::last_name = last_name;
+}
+
+void	Contact::set_nickname(std::string nickname)
+{
+	Contact::nickname = nickname;
+}
+
+void	Contact::set_phone_nbr(std::string phone_nbr)
+{
+	Contact::phone_nbr = phone_nbr;
+}
+
+void	Contact::set_secret(std::string secret)
+{
+	Contact::secret = secret;
 }
 
 int	main(int argc, char **argv)
 {
+	std::string command;
+	Contact		contact;
+	Phonebook	Phonebook;
 	(void)argv;
 	if (argc != 1)
 	{
@@ -32,16 +65,29 @@ int	main(int argc, char **argv)
 	}
 	while (42)
 	{
-		Contact		contact;
-		std::string command;
 		std::cin >> command;
 		if (command == "ADD")
 		{
-			std::string name;
-			std::cin >> name;
-			contact.set_value(name);
-			std::cout << contact.get_value() << std::endl;
+			std::string value;
+			std::cout << "First Name:" << std::endl;
+			std::getline(std::cin, value);
+			std::cin >> value;
+			contact.set_first_name(value);
+			std::cout << "Last Name:" << std::endl;
+			std::cin >> value;
+			contact.set_last_name(value);
+			std::cout << "Nickname:" << std::endl;
+			std::cin >> value;
+			contact.set_nickname(value);
+			std::cout << "Phone number:" << std::endl;
+			std::cin >> value;
+			contact.set_phone_nbr(value);
+			std::cout << "Darkest Secret:" << std::endl;
+			std::cin >> value;
+			contact.set_secret(value);
 		}
+		if (command == "SEARCH")
+			contact.get_value();
 		if (command == "EXIT")
 			break ;
 	}
